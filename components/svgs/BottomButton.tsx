8accbf9d6949cb5afe50c5cc51ce10e07c9bad67
batuhan içöz <batuhan@icoz.co>
2022-05-17 09:00:34 +0300

    convert svg files to react components

---
+++ components/svgs/BottomButton.tsx
@@ -0,0 +1,10 @@
+export default function SVGBottomButton({ ...props }) {
+    return (
+        <svg
+            {...props} viewBox="0 0 32 77" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect x="2" y="2" width="27.9984" height="47.0882" rx="13.9992" stroke="#090909" stroke-width="4" />
+    <path d="M15.7714 11.1809L15.7714 20.8209" stroke="#090909" stroke-width="4" stroke-linecap="round" />
+    <path d="M6.59045 64.36L16.2304 74L25.8704 64.36" stroke="#090909" stroke-width="4" stroke-linecap="round" />
+    </svg>
+    );
+}
---
+++ components/svgs/Triangle.tsx
@@ -0,0 +1,8 @@
+export default function SVGTriangle({ ...props }) {
+    return (
+        <svg
+            {...props} viewBox="0 0 37 37" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16.768 3C17.5378 1.66666 19.4623 1.66667 20.2321 3L32.7894 24.75C33.5592 26.0833 32.597 27.75 31.0574 27.75H5.94263C4.40303 27.75 3.44078 26.0833 4.21058 24.75L16.768 3Z" fill="currentColor" />
+        </svg>
+    );
+}
